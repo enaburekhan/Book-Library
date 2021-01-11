4e208e4e338cf59bb9b44f56ad7c6ff81700a40d
haywhizzz <haywhizzz@gmail.com>
2021-01-11 09:49:44 +0100

    Add book function

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ const readStatus = document.getElementById('status');
 const bookcontainer = document.querySelector('.bookcont');
 const errormsg = document.querySelector('#error');
 const successmsg = document.querySelector('#success');
+
+const myBooks = [{
+    name: 'Ruby deep dive',
+    author: 'Jesus Castello',
+    pages: 203,
+    status: 'Finished',
+}];​
+function Book(name, author, pages, status) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
