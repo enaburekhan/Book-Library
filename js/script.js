04b30f44f52a77c20636be2751a54f7dd4333f5f
enaburekhan <ericenaburekhan2018@gmail.com>
2021-01-11 09:45:00 +0100

    create javascript variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const addBook = document.querySelector('.addbook');
+const formCtrl = document.querySelector('.form-ctrl');
+const maincont = document.querySelector('.maincont');
+const btnclose = document.querySelector('.close');
+const submit = document.querySelector('.submit-btn');
+const bookName = document.getElementById('bookName');
+const bookAuthor = document.getElementById('bookAuthor');
+const numPages = document.getElementById('pages');
+const readStatus = document.getElementById('status');
+const bookcontainer = document.querySelector('.bookcont');
+const errormsg = document.querySelector('#error');
+const successmsg = document.querySelector('#success');
